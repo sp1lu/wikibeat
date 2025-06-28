2988716741acfa8e4a3d33e41b47fb94f785f49d
sp1lu <davide.rivolta@yahoo.it>
2025-06-28 13:40:55 +0200

    Relative url in import

--- src/data/audios.ts
+++ src/data/audios.ts
@@ -1,8 +1,8 @@
 export const AUDIOS: Map<number, string> = new Map([
-    [1, '/audio/C2v10.mp3'],
-    [2, '/audio/C3v10.mp3'],
-    [3, '/audio/C4v10.mp3'],
-    [4, '/audio/C5v10.mp3'],
-    [5, '/audio/C6v10.mp3'],
-    [6, '/audio/C7v10.mp3'],
+    [1, `${import.meta.env.BASE_URL}/audio/C2v10.mp3`],
+    [2, `${import.meta.env.BASE_URL}/audio/C3v10.mp3`],
+    [3, `${import.meta.env.BASE_URL}/audio/C4v10.mp3`],
+    [4, `${import.meta.env.BASE_URL}/audio/C5v10.mp3`],
+    [5, `${import.meta.env.BASE_URL}/audio/C6v10.mp3`],
+    [6, `${import.meta.env.BASE_URL}/audio/C7v10.mp3`],
 ]);
